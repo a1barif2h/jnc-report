970e42a16a5e7419de59995343d326ee9c1e5d3f
ArnabPaul23Joy <arnabpaul65f826@gmail.com>
2021-11-21 13:51:04 +0600

    import permit pdf generator

--- services/certificateGeneratorFactory.js
+++ services/certificateGeneratorFactory.js
@@ -14,13 +14,18 @@ const generate = async (data) => {
     let title = data.title;
     let generatedPdf = null;
     switch (title) {
-        case 'Project Clearance':
-            const projectClearance = new ProjectClearance();
-            console.log("Generating pdf for project Clearance")
-            generatedPdf = await projectClearance.generate(data);
-            break;
-        default:
-            break;
+      case "Project Clearance":
+        const projectClearance = new ProjectClearance();
+        console.log("Generating pdf for project Clearance");
+        generatedPdf = await projectClearance.generate(data);
+        break;
+      case "Import Permit  v2":
+        const importPermit = new ImportPermit();
+        console.log("Generating pdf for Import Permit  v2");
+        generatedPdf = await importPermit.generate(data);
+        break;
+      default:
+        break;
     }
     return generatedPdf;
 }
