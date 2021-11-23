efbcfcd3de6b4318b617a87ba48b219bbdd8b12d
Mohammad Nazmul Hasan <nhtonmoy.cse@gmail.com>
2021-11-23 12:50:46 +0600

    working on Export

--- pdf_generators/ExportPermit.js
+++ pdf_generators/ExportPermit.js
@@ -4,9 +4,22 @@ const htmlTemplate = fs.readFileSync(
   "./pdf_templates/export-permit/export-permit.html",
   "utf8"
 );
+const multipageHeader =fs.readFileSync(
+  "./pdf_templates/export-permit/export-multipage-header.html"
+);
+
+const materialGroup =fs.readFileSync(
+  "./pdf_templates/export-permit/export-material-group.html"
+);
+
+const htmlFooter = fs.readFileSync(
+  "./pdf_templates/export-permit/export-permit-footer.html"
+);
+
 const pdf = require("./PdfGenerator");
 const options = { format: "A4", orientation: "portrait" };
 const dateTimeFormattor = require('../util/dateTimeFormattor');
+const templateEngine = require('../util/templateEngine');
 
 class ExportPermit {
   constructor() {}
@@ -19,12 +32,28 @@ class ExportPermit {
     body.formValue.undertakingDate = dateTimeFormattor.getApplicationDate(body.formValue.undertakingDate);
     body.formValue.invoiceVendorRefDate = dateTimeFormattor.getApplicationDate(body.formValue.invoiceVendorRefDate);
 
-    body.formValue.dataGrid.forEach(element => {
-      console.log(element);
+    body.formValue.dataGrid.forEach((element, index) => {
+      let html = htmlTemplate;
+      console.log(index);
+      // console.log(element);
       element.issueDate = dateTimeFormattor.getApplicationDate(element.issueDate);
       element.expiryDate = dateTimeFormattor.getApplicationDate(element.expiryDate);
+      if(index==0){
+        element.firstMaterial = multipageHeader;
+        templateEngine.replacer(multipageHeader, element);
+      }
+      if(body.formValue.dataGrid.length < 1){
+        
+      }
+      if(index%2!=0){
+        html+="<div class=\"mainContainer pagebreak\">";
+        html+=multipageHeader;
+      }
     });
-    const response = await pdf.generatePdfFromHtml(htmlTemplate, body, options);
+
+    html += "</main></div>";
+    console.log(html);
+    const response = await pdf.generatePdfFromHtml(html, body, options);
     return response;
   }
 }
