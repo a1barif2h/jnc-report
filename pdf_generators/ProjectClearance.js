692756dcf074a665910600c29176953efa6bd348
Mohammad Nazmul Hasan <nhtonmoy.cse@gmail.com>
2022-01-17 18:37:46 +0600

    updated project clearance generate method

--- pdf_generators/ProjectClearance.js
+++ pdf_generators/ProjectClearance.js
@@ -16,7 +16,7 @@ class ProjectClearance {
         // body.fitnessIssueDate = body.hasOwnProperty("fitnessIssueDate") ? body.fitnessIssueDate : "_";
         // pdf.pdfGenerator(htmlTemplate, body, res, options)
         // body.formValue.backgroundImg = background_image;
-        body.formValue.backgroundImg = background_cancelled;
+        body.formValue.backgroundImg = background_image;
         const response = await pdf.generatePdfFromHtml(htmlTemplate, body, options);
         return response;
     }
