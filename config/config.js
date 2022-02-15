e504085d2be3fe6d97a9125cd31e37efbc673815
arnab_paul_joy <arnabpaul65f826@gmail.com>
2022-02-15 18:29:03 +0600

    fixed the land use plan module

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const config = convict({
 
     env: {
         format: ['production', 'staging', 'dev', 'default'],
-        default: 'dev',
+        default: 'default',
         arg: 'nodeEnv',
         env: 'NODE_ENV'
     }
