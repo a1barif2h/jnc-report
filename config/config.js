a35a0974efe7e0f6e2ac49d7d2c98fe260905f86
arnab_paul_joy <arnabpaul65f826@gmail.com>
2022-03-01 17:22:17 +0600

    staging merged with dev

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const config = convict({
 
     env: {
         format: ['production', 'staging', 'dev', 'default'],
-        default: 'dev',
+        default: 'staging',
         arg: 'nodeEnv',
         env: 'NODE_ENV'
     }
