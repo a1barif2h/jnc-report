ce9ea6275808bc06b2c876f72b725dc98e01bf15
arnab_paul_joy <arnabpaul65f826@gmail.com>
2022-03-12 17:26:47 +0600

    fixing the issues

--- services/certificateGeneratorService.js
+++ services/certificateGeneratorService.js
@@ -112,9 +112,9 @@ const generateCertificate = async (req) => {
                                 if(key=="dataGrid1"){
                                     console.log("\n\n\n"+commonFieldValue[key].length+"\n\n\n")
                                 }
-                                if(commonFieldValue[key]!=null && !Object.keys(res.formValue).includes(key)){
+                                if(commonFieldValue[key]!=null && !res.formValue.hasOwnProperty(key)){
                                     res.formValue[key] = commonFieldValue[key]
-                                } 
+                                }
                             })
 
                             bufferResponse = await certificateGeneratorFactory.generate(res);
