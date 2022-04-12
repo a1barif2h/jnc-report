5f59216a2ac59c7c4f67b5fc4e20bbb6d248004c
a1barif2h <a1barif2h@gmail.com>
2022-04-12 12:55:45 +0600

    OIS-622 'In certificate Proposed date allignment is not okay.' solved

--- services/certificateGeneratorService.js
+++ services/certificateGeneratorService.js
@@ -12,26 +12,6 @@ const fs = require("fs");
 const encryption = require("../util/encryption");
 const background_image = fs.readFileSync('./pdf_templates/background_image.html',"utf8");
 const background_cancelled = fs.readFileSync('./pdf_templates/background_cancelled.html',"utf8");
-// const Cryptr = require("cryptr");
-// const cryptr = new Cryptr(process.env.SECRET_KEY);
-// Server will call this File
-// bezaservicegateway
-// method(sopCode)
-//   let certificateGenerator = factoryMethod(sopCode);
-
-
-
-//   bezaservicegateway.getFormValue()
-//     .then(formValue => {
-//         return certificateGenerator.generate(formvalue);
-//     })
-//     .then(buffer => {
-//         return bezaservicegateway.upload();
-//     })
-//     .then( url => {
-//         return bezaservicegateway.saveCertificateInfo
-//     });
-
 
 const generateQR = async text => {
     try {
@@ -42,21 +22,14 @@ const generateQR = async text => {
   }
 
 const generateBarcode = async text => {
-    // JsBarcode(canvas, text, {
-    //     width: 1,
-    //     displayValue: false
-    // })
-    
-    // const barcodeData = canvas.toDataURL('image/png')
     JsBarcode(svgNode, text, {
         xmlDocument: document,
-        width: 0.25,
+        width: 0.75,
         height: 25,
         displayValue: false
     });
     
     const barcodeData = xmlSerializer.serializeToString(svgNode);
-    // console.log(barcodeData);
     return barcodeData;
 }
 
@@ -69,23 +42,12 @@ const generateCertificate = async (req) => {
     await bezaServiceGateway
                     .getFormValueByApplicationID(req.body.applicationId).then(
                        async res=>{
-                            const canvas = {}
                             const appId = encryption.encrypt(""+req.body.applicationId);
-                           // req.body.applicationId;
-                            // const encryptedUserSopId = cryptr.encrypt(appId);
-                            // console.log(
-                            //   "encryptedUserSopId:  " + encryptedUserSopId
-                            // );
-                            // console.log(
-                            //   "decryptedUserSopId:  " +
-                            //     cryptr.decrypt(encryptedUserSopId)
-                            // );
+                            
                             colonOrNot = config.backendApi.bezaServiceFrontEndPort == "" ? "" : ":";
                             const url =
                               `${config.backendApi.bezaServiceBaseUrl}${colonOrNot}${config.backendApi.bezaServiceFrontEndPort}/validate-certificate?applicationId=` +
                               appId;
-                            
-                            
                             await generateQR(url).then(qrRes=> res.formValue.qrcode = qrRes).catch(err=> console.log(err));
 
                             await generateBarcode(res.trackingId).then (barRes => res.formValue.barcode = barRes).catch(err=> console.log(err));
@@ -102,11 +64,8 @@ const generateCertificate = async (req) => {
                             /**
                              * merging the common fields
                              */
-                            console.log("req.body.investorId:   "+req.body.investorId)
                             const commonFieldValue=await bezaServiceGateway.getCommonFileds(req.body.investorId);
-                            console.log("\n\n\n\n\n\ncommonFieldValue:   "+JSON.stringify(commonFieldValue))
                             let commonFieldValueKeys = Object.keys(commonFieldValue);
-                            console.log("\n\n\n\n\n\ncommonFieldValueKeys:   "+commonFieldValueKeys.toString())
                             commonFieldValueKeys.forEach(key=>{
                                 console.log("key:     "+key)
                                 if(key=="dataGrid1"){
