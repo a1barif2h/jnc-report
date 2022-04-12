ece6b5666c03c41bd500a732fb6bcbc465406ebc
a1barif2h <a1barif2h@gmail.com>
2022-04-12 10:24:12 +0600

    change variable name

--- services/gateway_services/bezaServiceGateway.js
+++ services/gateway_services/bezaServiceGateway.js
@@ -117,17 +117,18 @@ const getPaymentVoucherInfo = async ({applicationId}) => {
   config.backendApi.bezaServicePort+ config.backendApi.paymentVoucherInfoPath + applicationId
   
   // THIS FUNCTION IS RESPONSE TO CONVERT NUMBER TO AMOUNT OF WORD.
-  const a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-  const b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+  const digitValueOne = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+  const digitValueTwo = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
   function inWords (num) {
       if ((num = num.toString()).length > 9) return 'overflow';
       n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-      if (!n) return; var str = '';
-      str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
-      str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
-      str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
-      str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
-      str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + '' : '';
+      if (!n) return; 
+      let str = '';
+      str += (n[1] != 0) ? (digitValueOne[Number(n[1])] || digitValueTwo[n[1][0]] + ' ' + digitValueOne[n[1][1]]) + 'crore ' : '';
+      str += (n[2] != 0) ? (digitValueOne[Number(n[2])] || digitValueTwo[n[2][0]] + ' ' + digitValueOne[n[2][1]]) + 'lakh ' : '';
+      str += (n[3] != 0) ? (digitValueOne[Number(n[3])] || digitValueTwo[n[3][0]] + ' ' + digitValueOne[n[3][1]]) + 'thousand ' : '';
+      str += (n[4] != 0) ? (digitValueOne[Number(n[4])] || digitValueTwo[n[4][0]] + ' ' + digitValueOne[n[4][1]]) + 'hundred ' : '';
+      str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (digitValueOne[Number(n[5])] || digitValueTwo[n[5][0]] + ' ' + digitValueOne[n[5][1]]) + '' : '';
       return str;
   }
 
