e46d0eb1a8d22922d3b35e3d5437d81bddcc79cd
a1barif2h <a1barif2h@gmail.com>
2022-04-23 23:16:34 +0600

    all done

--- services/gateway_services/bezaServiceGateway.js
+++ services/gateway_services/bezaServiceGateway.js
@@ -123,11 +123,24 @@ const getPaymentVoucherInfo = async ({applicationId}) => {
   }
 }
 
+const getCertificateInfo = async (applicationId) => {
+  const url = config.backendApi.bezaServiceBaseUrl+  ":" +
+  config.backendApi.bezaServicePort+ config.backendApi.certificateInfoPath + applicationId
+
+  try {
+    const {data} = await axios.get(url);
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 
 module.exports = {
   getFormValueByApplicationID,
   upload,
   saveCertificateInfo,
   getCommonFileds,
-  getPaymentVoucherInfo
+  getPaymentVoucherInfo,
+  getCertificateInfo
 };
