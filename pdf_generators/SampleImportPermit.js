e53d8302660582a43add4e7f23093d1a4a2ef037
a1barif2h <a1barif2h@gmail.com>
2022-05-08 14:03:26 +0600

    OIS-760 Certificate: Sample Import Permit issue fixed

--- pdf_generators/SampleImportPermit.js
+++ pdf_generators/SampleImportPermit.js
@@ -13,6 +13,11 @@ class SampleImportPermit {
   constructor() {}
 
   async generate(body) {
+    // CHANGE DATE FORMATE
+    body.formValue.invoiceDate = dateTimeFormattor.getFormatDate(body.formValue.invoiceDate);
+    body.formValue.issueDate = dateTimeFormattor.getFormatDate(body.formValue.issueDate);
+    body.formValue.endTime = dateTimeFormattor.getFormatDate(body.formValue.endTime);
+    
     let htmlTemplate = fs.readFileSync(
       "./pdf_templates/sample-import-permit/sample-import-permit.html",
       "utf8"
@@ -53,7 +58,6 @@ class SampleImportPermit {
       headerTemplate
     );
 
-    console.log("==========================================");
     materialsDetailsTemplate = materialsDetailsTemplateInitial;
     htmlImportTemplate = materialsDescriptionParser.addRemainingMaterials(
       body.formValue.sampleImportMaterialsInformationGroup,
@@ -61,11 +65,6 @@ class SampleImportPermit {
       htmlImportTemplate,
       headerTemplate
     );
-    console.log("htmlSampleImportTemplate:");
-    // console.log(htmlImportTemplate);
-    // if (body.formValue.dataGrid.length!=1){
-      
-    // }
     const response = await pdf.generatePdfFromHtmlMultipleMaterialDescription(
       htmlImportTemplate,
       options
@@ -74,139 +73,5 @@ class SampleImportPermit {
     return response;
   }  
 }
-
-// const addRemainingMaterials = function (
-//   dataGrid,
-//   materialsDetailsTemplate,
-//   htmlSampleImportPermit,
-//   headerTemplate
-// ) {
-//   if (dataGrid.length == 1) {
-//     htmlSampleImportPermit = htmlSampleImportPermit.replace(
-//       `{{remainingMaterialsDetails}}`,
-//       ""
-//     );
-//     return htmlSampleImportPermit;
-//   }
-//   let remainingMaterialsDetails = generateMultipleMaterialsDescription(
-//     dataGrid,
-//     materialsDetailsTemplate,
-//     headerTemplate
-//   );
-//   htmlSampleImportPermit = htmlSampleImportPermit.replace(
-//     `{{remainingMaterialsDetails}}`,
-//     remainingMaterialsDetails || "-"
-//   );
-
-//   console.log("==========================================\n\n\n");
-//   console.log("remainingMaterialsDetails:   " + remainingMaterialsDetails);
-//   return htmlSampleImportPermit;
-// };
-
-
-// const addFirstMaterials = function (
-//   dataGrid,
-//   materialsDetailsTemplate,
-//   htmlSampleImportPermit,
-//   headerTemplate
-// ) {
-//   if (dataGrid.length != 1) {
-//     materialsDetailsTemplate = materialsDetailsTemplate.replace(
-//       `{{footerHere}}`,
-//       ""
-//     );
-//   } else {
-//     materialsDetailsTemplate = materialsDetailsTemplate.replace(
-//       `{{footerHere}}`,
-//       footerTemplate.toString() || "-"
-//     );
-//   }
-//   materialsDetailsTemplate = materialsDetailsTemplate.replace(
-//     `{{headerHere}}`,
-//     ""
-//   );
-//   let firstMaterialsDetails = materialsDescriptionParser.parseJasonIntoHtml(
-//     dataGrid[0],
-//     materialsDetailsTemplate
-//   );
-//   htmlSampleImportPermit = htmlSampleImportPermit.replace(
-//     `{{firstMaterialsDetails}}`,
-//     firstMaterialsDetails || "-"
-//   );
-//   console.log("matarialDescription:   " + firstMaterialsDetails);
-//   return htmlSampleImportPermit;
-// };
-
-// const parseJasonIntoHtml = function (json, template) {
-//   for (var key in json) {
-//     template = template.replaceAll("{{" + key + "}}", json[key] || "-");
-//   }
-//   return template;
-// };
-
-// const generateMultipleMaterialsDescription = function (
-//   dataGrid,
-//   materialsTemplate,
-//   headerTemplate
-// ) {
-//   // if (data.length == 0) {
-//   //   return "";
-//   // }
-//   // const dataGrid = data.dataGrid;
-//   let materialsDescriptionTemplate = "";
-//   // materialsDescriptionTemplate += parseJasonIntoHtml(
-//   //   dataGrid[0],
-//   //   materialsTemplate
-//   // );
-//   const pageBreak = '<div class="mainContainer pageBreak">';
-//   for (let i = 1; i < dataGrid.length; i++) {
-//     let tempmaterialsTemplate = materialsTemplate;
-//     if (i == dataGrid.length - 1) {
-//       console.log("iiiiiiiiiiii:   " + footerTemplate);
-//       tempmaterialsTemplate = tempmaterialsTemplate.replace(
-//         `{{footerHere}}`,
-//         footerTemplate.toString() || "-"
-//       );
-//     } else {
-//       tempmaterialsTemplate = tempmaterialsTemplate.replace(
-//         `{{footerHere}}`,
-//         ""
-//       );
-//     }
-//     if (i % 3 == 1) {
-//       materialsDescriptionTemplate += pageBreak;
-
-//       tempmaterialsTemplate = tempmaterialsTemplate.replace(
-//         `{{headerHere}}`,
-//         headerTemplate.toString()
-//       );
-//       console.log("pagebreak added!  " + i);
-//     } else {
-//       tempmaterialsTemplate = tempmaterialsTemplate.replace(
-//         `{{headerHere}}`,
-//         ""
-//       );
-//     }
-//     materialsDescriptionTemplate += materialsDescriptionParser.parseJasonIntoHtml(
-//       dataGrid[i],
-//       tempmaterialsTemplate
-//     );
-
-//     if (i % 3 == 0) {
-//       materialsDescriptionTemplate += "</div>";
-//       console.log("</div>  added: " + i);
-//     }
-//   }
-
-//   // if (dataGrid.length >1) {
-
-//   // }
-//   if (dataGrid.length != 0 && dataGrid.length % 2 == 0) {
-//     materialsDescriptionTemplate += "</div>";
-//     console.log("</div>  added: " + dataGrid.length);
-//   }
-
-//   return materialsDescriptionTemplate;
-//};
 
 module.exports = SampleImportPermit;
