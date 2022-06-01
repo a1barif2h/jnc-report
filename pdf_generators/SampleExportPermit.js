1595deafcb7476e842ae23fe2a979da96070cb24
a1barif2h <a1barif2h@gmail.com>
2022-06-01 19:13:47 +0600

    done

--- pdf_generators/SampleExportPermit.js
+++ pdf_generators/SampleExportPermit.js
@@ -16,15 +16,15 @@ class SampleExportPermit {
 
     body.formValue.expiredDate = dateTimeFormattor.getFormatDate(body.formValue.expiredDate);
     // HANDLE EXPIRE DATE AND PLACE OF ISSUE
-    if (body.formValue.carrierType !== 'Hand Carry') {
-      body.formValue.expiredDate = '';
-    }
-    if(body.formValue.issueDate) {
-      body.formValue.issueDate = `<div>
-      <small>Issue date :</small>
-      <p>${body.formValue.issueDate}</p>
-    </div>`
-    }
+    // if (body.formValue.carrierType !== 'Hand Carry') {
+    //   body.formValue.expiredDate = '';
+    // }
+    // if(body.formValue.issueDate) {
+    //   body.formValue.issueDate = `<div>
+    //   <small>Issue date :</small>
+    //   <p>${body.formValue.issueDate}</p>
+    // </div>`
+    // }
     let htmlTemplate = fs.readFileSync(
       "./pdf_templates/sample-export-permit/sample-export-permit.html",
       "utf8"
