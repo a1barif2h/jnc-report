4c1600cca63677c9110d6725b1cb3f9e7193e794
nazmul-prince <nazmulfarhan@gmail.com>
2022-08-16 21:43:29 +0600

    changed config to read from json

--- config/config.js
+++ config/config.js
@@ -57,32 +57,54 @@ const options = {
   }
 };
 
-(async function loadCfg() {
-    
-    console.log("Loading configuration for profile: " + activeProfile);
-    await client.load(options)
-        .then((cfg) => {
-            configProperties.BEZA_SERVICE_BASE_URL = cfg.get('beza.service.base.url');
-            configProperties.BEZA_SERVICE_PORT = cfg.get('beza.service.base.port');
-            configProperties.BEZA_FRONT_END_BASE_URL = cfg.get('beza.service.front_end.base.url');
-            configProperties.BEZA_FRONT_END_PORT = cfg.get('beza.service.front_end.port');
-            configProperties.BEZA_SERVICE_MAYAN_UPLOAD_PATH = cfg.get('beza.service.mayan_upload.path');
-            configProperties.BEZA_SERVICE_FORM_BY_APPLICATION_ID_PATH = cfg.get('beza.service.form_value_by_application_id.path');
-            configProperties.BEZA_SERVICE_CERTIFICATE_INFO_PATH = cfg.get('beza.service.certificate_info.path');
-            configProperties.BEZA_SERVICE_COMMON_FIELDS_PATH = cfg.get('beza.service.commom_fields.path');
-            configProperties.BEZA_SERVICE_PAYMENT_VOUCHER_PATH = cfg.get('beza.service.payment_voucher_info.path');
-            configProperties.BEZA_SERVICE_CERTIFICATE_INFO_PATH = cfg.get('beza.service.certificate_info.path');
-            configProperties.BEZA_SERVICE_DESK_USER_SIGNATURE_PATH = cfg.get('beza.service.desk_user_signature.path');
-            configProperties.BEZA_SERVICE_CONVERT_CURRENCY_PATH = cfg.get('beza.service.convert_currency.path');
-            configProperties.KEYCLOAK_BASE_URL = cfg.get('keycloak.base.url');
-            configProperties.KEYCLOAK_PORT = cfg.get('keyloak.port');
-            configProperties.KEYCLOAK_USER_INFO_PATH = cfg.get('keyloak.user_info.path');
-        })
-        .catch((error) => console.error(error));
+const conf = config.getProperties();
+configProperties.BEZA_SERVICE_BASE_URL = conf.backendApi.bezaServiceBaseUrl;
+configProperties.BEZA_SERVICE_PORT = conf.backendApi.bezaServicePort;
+configProperties.BEZA_FRONT_END_BASE_URL = conf.backendApi.bezaServiceFrontEndBaseUrl;
+configProperties.BEZA_FRONT_END_PORT = conf.backendApi.bezaServiceFrontEndPort;
+configProperties.BEZA_SERVICE_MAYAN_UPLOAD_PATH = conf.backendApi.mayanCertificateUploadPath;
+configProperties.BEZA_SERVICE_FORM_BY_APPLICATION_ID_PATH = conf.backendApi.bezaServiceGetFormValuesByApplicationIdPath;
+configProperties.BEZA_SERVICE_CERTIFICATE_INFO_PATH = conf.backendApi.bezaServiceGetCertificateInfoPath;
+configProperties.BEZA_SERVICE_COMMON_FIELDS_PATH = conf.backendApi.bezaServiceCommmonFields;
+configProperties.BEZA_SERVICE_PAYMENT_VOUCHER_PATH = conf.backendApi.paymentVoucherInfoPath;
+configProperties.BEZA_SERVICE_CERTIFICATE_INFO_PATH = conf.backendApi.certificateInfoPath;
+configProperties.BEZA_SERVICE_DESK_USER_SIGNATURE_PATH = conf.backendApi.bezaServiceDeskUserSignature;
+configProperties.BEZA_SERVICE_CONVERT_CURRENCY_PATH = conf.backendApi.convertCurrency;
+configProperties.KEYCLOAK_BASE_URL = conf.backendApi.keycloakBaseUrl;
+configProperties.KEYCLOAK_PORT = conf.backendApi.keycloakPort;
+configProperties.KEYCLOAK_USER_INFO_PATH = conf.backendApi.keycloakUserInfoPath;
 
-    console.log("Loaded propertes: ");
-    console.log(configProperties);
-})();
+// (async function loadCfg() {
+    
+//     console.log("Loading configuration for profile: " + activeProfile);
+//     await client.load(options)
+//         .then((cfg) => {
+//             configProperties.BEZA_SERVICE_BASE_URL = cfg.get('beza.service.base.url');
+//             configProperties.BEZA_SERVICE_PORT = cfg.get('beza.service.base.port');
+//             configProperties.BEZA_FRONT_END_BASE_URL = cfg.get('beza.service.front_end.base.url');
+//             configProperties.BEZA_FRONT_END_PORT = cfg.get('beza.service.front_end.port');
+//             configProperties.BEZA_SERVICE_MAYAN_UPLOAD_PATH = cfg.get('beza.service.mayan_upload.path');
+//             configProperties.BEZA_SERVICE_FORM_BY_APPLICATION_ID_PATH = cfg.get('beza.service.form_value_by_application_id.path');
+//             configProperties.BEZA_SERVICE_CERTIFICATE_INFO_PATH = cfg.get('beza.service.certificate_info.path');
+//             configProperties.BEZA_SERVICE_COMMON_FIELDS_PATH = cfg.get('beza.service.commom_fields.path');
+//             configProperties.BEZA_SERVICE_PAYMENT_VOUCHER_PATH = cfg.get('beza.service.payment_voucher_info.path');
+//             configProperties.BEZA_SERVICE_CERTIFICATE_INFO_PATH = cfg.get('beza.service.certificate_info.path');
+//             configProperties.BEZA_SERVICE_DESK_USER_SIGNATURE_PATH = cfg.get('beza.service.desk_user_signature.path');
+//             configProperties.BEZA_SERVICE_CONVERT_CURRENCY_PATH = cfg.get('beza.service.convert_currency.path');
+//             configProperties.KEYCLOAK_BASE_URL = cfg.get('keycloak.base.url');
+//             configProperties.KEYCLOAK_PORT = cfg.get('keyloak.port');
+//             configProperties.KEYCLOAK_USER_INFO_PATH = cfg.get('keyloak.user_info.path');
+//         })
+//         .catch((error) => console.error(error));
+
+//     console.log("Loaded propertes: ");
+//     console.log(configProperties);
+// })();
+
+
+console.log("Loading configuration for profile: " + activeProfile);
+console.log("Loaded propertes: ");
+console.log(configProperties);
 
 module.exports = {
     oldConfig: config.getProperties(),
