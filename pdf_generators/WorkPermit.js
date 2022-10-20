0138b6788431e00b69bb39079a37fcc8cdec2d78
a1barif2h <a1barif2h@gmail.com>
2022-10-20 17:35:46 +0600

    change logic for key remove

--- pdf_generators/WorkPermit.js
+++ pdf_generators/WorkPermit.js
@@ -47,10 +47,11 @@ class WorkPermit {
     );
     let remunerationBoxHtmlTemplate = fs.readFileSync("./pdf_templates/work-permit/remuneration-box.html", "utf8")
     remunerationBoxHtmlTemplate = replacer(remunerationBoxHtmlTemplate, body.formValue);
-    // THIS LOGIC HAS PREVIOUS REQUERMENT 
-    // if (body.formValue.typeOfVisaObtainedForTheIncumbentForeignNationals !== "E - Employment Visa") {
+    // THIS IS NEW REQUIREMENT LOGIC 
+    const type_visa = body.formValue.typeOfVisaObtainedForTheIncumbentForeignNationals;
+    if (type_visa !== "E - Employment Visa" || type_visa !== "PI - Private Investor Visa") {
       remunerationBoxHtmlTemplate = keyRemover(remunerationBoxHtmlTemplate); // THIS LINE NEED TO CLEAN PREVIOUS VISA TYPE KEY
-    // }
+    }
     body.formValue.remunerationBox = remunerationBoxHtmlTemplate;
 
     const response = await pdf.generatePdfFromHtml(htmlTemplate, body, options);
