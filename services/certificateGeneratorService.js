35f93e9de664b5b2f20e04fc7c21aaa9f669e0dd
Mohammad Nazmul Hasan <nhtonmoy.cse@gmail.com>
2023-01-30 19:18:03 +0600

    small fix

--- services/certificateGeneratorService.js
+++ services/certificateGeneratorService.js
@@ -79,7 +79,7 @@ const generateCertificate = async (req) => {
                                 res.formValue.certificateGenerateDate = certificateGenerateDate;
                                 res.formValue.validTill = dateTimeFormattor.getValidTillDate(certificateGenerateDate);
                                 res.formValue.cancellationDate = res.hasOwnProperty('approvalDate') ?
-                                             ("Cancellation Date : "+ dateTimeFormattor.getFormatDate(res.approvalDate)) : "";
+                                             ("Cancellation Date : "+ dateTimeFormattor.getFormatDate(res.approvalDate)) : " ";
                             }
                             else if(req.body.isRevoke){
                                 console.log("SETTING BACKGROUND FOR REVOKE");
@@ -88,7 +88,7 @@ const generateCertificate = async (req) => {
                                 const certificateGenerateDate = dateTimeFormattor.getFormatDate(certificateInfo.createdAt)
                                 res.formValue.certificateGenerateDate = certificateGenerateDate;
                                 res.formValue.validTill = dateTimeFormattor.getValidTillDate(certificateGenerateDate);
-                                res.formValue.cancellationDate = "";
+                                res.formValue.cancellationDate = " ";
                             }
                             else{
                                 console.log("SETTING BACKGROUND");
@@ -97,7 +97,7 @@ const generateCertificate = async (req) => {
                                 const certificateGenerateDate = dateTimeFormattor.getFormatDate(new Date(res.approvalDate))
                                 res.formValue.certificateGenerateDate = certificateGenerateDate;
                                 res.formValue.validTill = dateTimeFormattor.getValidTillDate(certificateGenerateDate)
-                                res.formValue.cancellationDate = "";
+                                res.formValue.cancellationDate = " ";
                             }
                             res.formValue.trackingId = res.trackingId;
                             res.formValue.applicationDate = dateTimeFormattor.getApplicationDate(new Date(res.submittedDate).toLocaleDateString());
