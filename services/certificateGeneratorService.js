2430573936fbf3ad17944c4dee4de9cd2f655345
Mohammad Nazmul Hasan <nhtonmoy.cse@gmail.com>
2023-02-19 16:16:16 +0600

    added amendment date in all certificates

--- services/certificateGeneratorService.js
+++ services/certificateGeneratorService.js
@@ -98,14 +98,23 @@ const generateCertificate = async (req) => {
                                 res.formValue.certificateGenerateDate = res.formValue.hasOwnProperty('lastAmendmentDate') 
                                                                         ? dateTimeFormattor.getFormatDate(res.formValue.lastAmendmentDate)
                                                                         : certificateGenerateDate;
-                                res.formValue.validTill = dateTimeFormattor.getValidTillDate(certificateGenerateDate)
+                                res.formValue.validTill = res.formValue.hasOwnProperty('lastAmendmentDate')
+                                                            ? dateTimeFormattor.getValidTillDate(res.formValue.lastAmendmentDate)
+                                                            : dateTimeFormattor.getValidTillDate(certificateGenerateDate);
                                 res.formValue.cancellationDate = " ";
                             }
                             res.formValue.trackingId = res.trackingId;
                             res.formValue.applicationDate = dateTimeFormattor.getApplicationDate(new Date(res.submittedDate).toLocaleDateString());
 
-                            res.formValue.lastAmendmentDate = res.formValue.hasOwnProperty('lastAmendmentDate') ?
-                                             "Amendment date : "+ dateTimeFormattor.getFormatDate(res.approvalDate) : " ";                     
+                                                 
+
+                            if(res.sopCode === "ROYALTY_FEE" || res.sopCode === "TECHNICAL_KNOW_HOW_FEE") {
+                                res.formValue.lastAmendmentDate = res.formValue.hasOwnProperty('lastAmendmentDate') ?
+                                '<div class="content-info"><p class="content-label">Amendment Date:</p><p class="content-value">'+dateTimeFormattor.getFormatDate(res.approvalDate)+'</p></div>' : ' ';
+                            } else {
+                                res.formValue.lastAmendmentDate = res.formValue.hasOwnProperty('lastAmendmentDate') ?
+                                             "Amendment date : "+ dateTimeFormattor.getFormatDate(res.approvalDate) : " ";
+                            }
                             
                             /**
                              * merging the common fields
