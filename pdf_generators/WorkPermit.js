b1fe210627839318a476c8b135e220fce698d877
Mohammad Nazmul Hasan <nhtonmoy.cse@gmail.com>
2023-02-22 13:34:42 +0600

    reverted changes

--- pdf_generators/WorkPermit.js
+++ pdf_generators/WorkPermit.js
@@ -43,7 +43,10 @@ class WorkPermit {
     let type_visa = formValue.typeOfVisaObtainedForTheIncumbentForeignNationals;
     let eType = "E - Employment Visa";
     let piType = "PI - Private Investor Visa";
+    // let a3Type = "A3 - Work on Government Projects Visa";
+    // let eiType = "EI - Employment Type -1 Visa";
 
+    // return type_visa === eType || type_visa === piType || type_visa === a3Type || type_visa === eiType;
     return type_visa === eType || type_visa === piType;
   }
 
@@ -69,10 +72,16 @@ class WorkPermit {
     remunerationBoxHtmlTemplate = replacer(remunerationBoxHtmlTemplate, body.formValue);
     // THIS IS NEW REQUIREMENT LOGIC 
     const type_visa = body.formValue.typeOfVisaObtainedForTheIncumbentForeignNationals;
-    if (type_visa !== "E - Employment Visa" || type_visa !== "PI - Private Investor Visa") {
+    let eType = "E - Employment Visa";
+    let piType = "PI - Private Investor Visa";
+    // let a3Type = "A3 - Work on Government Projects Visa";
+    // let eiType = "EI - Employment Type -1 Visa";
+    // if (type_visa !== eType || type_visa !== piType || type_visa !== a3Type || type_visa !== eiType) {
+    if (type_visa !== eType || type_visa !== piType) {
       remunerationBoxHtmlTemplate = keyRemover(remunerationBoxHtmlTemplate); // THIS LINE NEED TO CLEAN PREVIOUS VISA TYPE KEY
       remunerationBoxHtmlTemplate = doubleNaTextRemover(remunerationBoxHtmlTemplate)
     }
+    // remunerationBoxHtmlTemplate = NaDashTextRemover(remunerationBoxHtmlTemplate)
     body.formValue.remunerationBox = remunerationBoxHtmlTemplate;
 
     const response = await pdf.generatePdfFromHtml(htmlTemplate, body, options);
