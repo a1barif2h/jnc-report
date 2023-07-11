94ba1c75a4bbf94c1c2c4c64512e37d8d97aa9ad
rhythm <rhysha.bd@gmail.coom>
2023-07-11 18:25:04 +0600

    test changes

--- pdf_generators/PdfGenerator.js
+++ pdf_generators/PdfGenerator.js
@@ -75,7 +75,7 @@ const ejsPuppeteerPdfGenerator = async (generateTemplate, options, pageStyle) =>
    */
   const browser = await puppeteer.launch(
     {
-      executablePath: "/usr/bin/chromium"
+      executablePath: "/usr/bin/google-chrome-stable"
     }
   );
   const page = await browser.newPage();
