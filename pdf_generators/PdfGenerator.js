8e4968036e852c8f05e15e87e4156a8ffecaf3b5
a1barif2h <a1barif2h@gmail.com>
2023-07-12 13:45:23 +0600

    modify puppeteer options

--- pdf_generators/PdfGenerator.js
+++ pdf_generators/PdfGenerator.js
@@ -75,7 +75,8 @@ const ejsPuppeteerPdfGenerator = async (generateTemplate, options, pageStyle) =>
    */
   const browser = await puppeteer.launch(
     {
-      executablePath: "/usr/bin/google-chrome-stable"
+      executablePath: "/usr/bin/google-chrome-stable",
+      args: ["--no-sandbox"],
     }
   );
   const page = await browser.newPage();
