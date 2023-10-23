37bd89a5790fa09c2efcfe29fb45bd93a3d65255
a1barif2h <a1barif2h@gmail.com>
2023-10-23 11:59:47 +0600

    adjust content for PR certificate

--- puppeteer_pdf_generators/ProjectRegistration.js
+++ puppeteer_pdf_generators/ProjectRegistration.js
@@ -108,7 +108,7 @@ class ImportPermit {
 
     const pageStyle = `
             @page {
-                margin-top: 150px;
+                margin-top: 130px;
                 margin-bottom: 40px;
             }
         `;
