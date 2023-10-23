a4df5738b4ed66418a327474c2cac0981bf1bb0e
a1barif2h <a1barif2h@gmail.com>
2023-10-23 11:40:30 +0600

    add machinery list in PR certificate and remove signeture

--- puppeteer_pdf_generators/ProjectRegistration.js
+++ puppeteer_pdf_generators/ProjectRegistration.js
@@ -1,61 +1,126 @@
-const fs = require('fs');
-const { ejsRender } = require('../util/templateEngine');
-const { getCommonOptions } = require('../util/utils');
-const { ejsPuppeteerPdfGenerator } = require('../pdf_generators/PdfGenerator');
-const ejsUtils = require('../util/ejsUtils');
-const { CARRIER_TYPE } = require('../constants/const');
-const { changeDateFormat } = require('../util/dateTimeFormattor');
-const { numberWithCommas } = require('../util/amountToWordUtil');
-const logger = require('../util/logger');
+const fs = require("fs");
+const { ejsRender } = require("../util/templateEngine");
+const { getCommonOptions } = require("../util/utils");
+const { ejsPuppeteerPdfGenerator } = require("../pdf_generators/PdfGenerator");
+const ejsUtils = require("../util/ejsUtils");
+const { CARRIER_TYPE } = require("../constants/const");
+const { changeDateFormat } = require("../util/dateTimeFormattor");
+const { numberWithCommas } = require("../util/amountToWordUtil");
+const logger = require("../util/logger");
+const { getMachenariesByApplicationID } = require("../services/gateway_services/bezaServiceGateway");
+const currencyConverter = require("../util/currencyConverter");
 
 class ImportPermit {
-    constructor() { };
+  constructor() {}
 
-    handleDateTimeFormat(formValue) {
-        //DATE TIME FORMAT: 13 August 2022
-        changeDateFormat(formValue, "applicationDate");
+  handleDateTimeFormat(formValue) {
+    //DATE TIME FORMAT: 13 August 2022
+    changeDateFormat(formValue, "applicationDate");
+  }
+
+  async getMachineries(
+    applicationId,
+    additionOfMachinery,
+    addMachineriesByFile
+  ) {
+    let machineriesToCalculate = [];
+    if (addMachineriesByFile) {
+      //machineries have been added through excel so get the values from there
+      const machineries = await getMachenariesByApplicationID(applicationId);
+      machineriesToCalculate = machineries.additionOfMachinery;
+    } else {
+      machineriesToCalculate = additionOfMachinery;
+    }
+    return machineriesToCalculate;
+  }
+
+  #getCurrencyList(machineriesToCalculate) {
+    const currencies = {};
+
+    machineriesToCalculate.length > 0 &&
+      machineriesToCalculate.map((machinery) => {
+        const currencyName = machinery.valueCurrency;
+        const currencyValue = parseFloat(machinery.valueInput);
+        if (currencies[currencyName]) {
+          currencies[currencyName] = currencies[currencyName] + currencyValue;
+        } else {
+          currencies[currencyName] = currencyValue;
+        }
+      });
+    return currencies;
+  }
+
+  handleAmountThousandsSeparator(formValue) {
+    logger.info(
+      "Start converting normal to thousands separator for sop: %s",
+      formValue.sopCode
+    );
+    formValue["dCostOfTheProjectInUs"] = numberWithCommas(
+      formValue["dCostOfTheProjectInUs"]
+    );
+
+    logger.info("Converting done");
+  }
+
+  async generate(body) {
+    this.handleDateTimeFormat(body.formValue);
+    this.handleAmountThousandsSeparator(body.formValue);
+
+    try {
+      const machineriesToCalculate = await this.getMachineries(
+        body?.id,
+        body.formValue?.additionOfMachinery,
+        body.formValue?.addMachineriesByFile
+      );
+      const currencyList = this.#getCurrencyList(machineriesToCalculate);
+
+      body.formValue.machineriesToCalculate = machineriesToCalculate;
+
+      const totalMachineryAmount = await currencyConverter(currencyList, "USD");
+      body.formValue.machineryCurrencyValue = totalMachineryAmount.toFixed(2);
+      body.formValue.machineryCurrency = "USD";
+      this.handleAmountThousandsSeparator(body.formValue);
+    } catch (error) {
+      logger.error(error);
     }
 
-    handleAmountThousandsSeparator(formValue) {
-        logger.info("Start converting normal to thousands separator for sop: %s", formValue.sopCode)
-        formValue["dCostOfTheProjectInUs"] = numberWithCommas(
-            formValue["dCostOfTheProjectInUs"]
-        );
+    body.formValue.industryCategory =
+      body.formValue?.industryCategory.name || "";
 
-        logger.info("Converting done")
-    }
+    body.formValue.utils = ejsUtils;
 
-    async generate(body) {
-        this.handleDateTimeFormat(body.formValue);
-        this.handleAmountThousandsSeparator(body.formValue)
+    const initialTemplate = fs.readFileSync(
+      "./ejs_pdf_templates/project-registration/project-registration.ejs",
+      "utf8"
+    );
+    const initialHeaderTemplate = fs.readFileSync(
+      "./ejs_pdf_templates/project-registration/header-template.ejs",
+      "utf8"
+    );
 
-        body.formValue.industryCategory =
-            body.formValue?.industryCategory.name || "";
+    const headerTemplate = ejsRender(initialHeaderTemplate, body);
+    const generateTemplate = ejsRender(initialTemplate, body);
 
-        body.formValue.utils = ejsUtils;
+    const options = {
+      ...getCommonOptions(body),
+      headerTemplate,
+    };
 
-        const initialTemplate = fs.readFileSync('./ejs_pdf_templates/project-registration/project-registration.ejs', 'utf8');
-        const initialHeaderTemplate = fs.readFileSync('./ejs_pdf_templates/project-registration/header-template.ejs', 'utf8');
-
-        const headerTemplate = ejsRender(initialHeaderTemplate, body);
-        const generateTemplate = ejsRender(initialTemplate, body);
-
-        const options = {
-            ...getCommonOptions(body),
-            headerTemplate,
-        };
-
-        const pageStyle = `
+    const pageStyle = `
             @page {
                 margin-top: 150px;
                 margin-bottom: 40px;
             }
         `;
 
-        const generatedPdf = ejsPuppeteerPdfGenerator(generateTemplate, options, pageStyle);
+    const generatedPdf = ejsPuppeteerPdfGenerator(
+      generateTemplate,
+      options,
+      pageStyle
+    );
 
-        return generatedPdf;
-    }
-};
+    return generatedPdf;
+  }
+}
 
 module.exports = ImportPermit;
